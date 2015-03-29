58d7c42b0623261565569e487173316ad8d36e78
chanderdeep <ch.yadav430@gmail.com>
2015-03-30 03:17:49 +1000

    Initial Commit

---
+++ src/Patient.java
@@ -0,0 +1,33 @@
+/**
+ * Created by akki on 3/30/2015.
+ */
+public class Patient {
+    private  String name;
+    private int age;
+    private  double height, weight;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDetails(double height,double weight){
+        this.height = height;
+        this.weight = weight;
+    }
+    public double getBMI(){
+        double BMI = weight/height;
+        return BMI;
+    }
+}
